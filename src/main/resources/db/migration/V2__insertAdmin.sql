0ad4511810a7d694e37d8401c5249f9b95384474
Suciu16 <115536547+Suciu16@users.noreply.github.com>
2023-04-19 21:54:17 +0300

    admin added into database (#5)

---
+++ src/main/resources/db/migration/V2__insertAdmin.sql
@@ -0,0 +1,19 @@
+ALTER TABLE admin
+ADD column user_id INT;
+
+ALTER table admin
+ADD constraint fk_admin
+foreign key (user_id) references admin (id);
+
+ALTER TABLE client
+    ADD column user_entity_id INT;
+
+ALTER table client
+    ADD constraint fk_client
+        foreign key (user_entity_id) references client (id);
+
+INSERT INTO global_user (username, email, password, user_role)
+VALUES ('admin', 'admin@mail.com', '1234', 'ROLE_ADMIN');
+
+INSERT INTO admin (id, admin_name, address)
+VALUES (1, 'admin', 'Cluj-Napoca');
