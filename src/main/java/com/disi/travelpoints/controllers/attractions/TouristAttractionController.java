76c8e003bb0c167f360e648aa8d8311b19e6728b
Alexandra Roșca <71495113+alexandra-rosca@users.noreply.github.com>
2023-04-22 18:20:36 +0300

    CRUD operation on touristic attractions

--- src/main/java/com/disi/travelpoints/controllers/attractions/TouristAttractionController.java
+++ src/main/java/com/disi/travelpoints/controllers/attractions/TouristAttractionController.java
@@ -1,7 +1,44 @@
 package com.disi.travelpoints.controllers.attractions;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.disi.travelpoints.dto.TouristAttractionDto;
+import com.disi.travelpoints.services.TouristAttractionService;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/tourist-attractions")
 public class TouristAttractionController {
+
+    private final TouristAttractionService touristAttractionService;
+
+    public TouristAttractionController(TouristAttractionService touristAttractionService) {
+        this.touristAttractionService = touristAttractionService;
+    }
+
+    @GetMapping()
+    public List<TouristAttractionDto> getAllAttractions() {
+        return touristAttractionService.getAllAttractions();
+    }
+
+    @PostMapping()
+    public TouristAttractionDto createAttraction(@RequestBody TouristAttractionDto touristAttractionDto) {
+        return touristAttractionService.createAttraction(touristAttractionDto);
+    }
+
+    @PutMapping("/{attractionId}")
+    public TouristAttractionDto updateAttraction(@RequestBody TouristAttractionDto touristAttractionDto, @PathVariable Integer attractionId) {
+        return touristAttractionService.updateAttraction(touristAttractionDto, attractionId);
+    }
+
+    @DeleteMapping("/{attractionId}")
+    public ResponseEntity<String> deleteTouristAttraction(@PathVariable @NotNull Integer attractionId) {
+        touristAttractionService.deleteAttraction(attractionId);
+        return new ResponseEntity<>("Tourist attraction deleted!", HttpStatus.OK);
+    }
+
+
 }
