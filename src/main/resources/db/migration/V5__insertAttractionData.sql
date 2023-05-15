582120e82344814a579fbab9a0fefeaec6135633
Alexandra Roșca <71495113+alexandra-rosca@users.noreply.github.com>
2023-05-15 18:01:40 +0300

    Creating script for populating attractions

---
+++ src/main/resources/db/migration/V5__insertAttractionData.sql
@@ -0,0 +1,67 @@
+UPDATE public.tourist_attraction
+SET "location"='France',
+    text_description='Iconic Parisian landmark, the Eiffel Tower is a towering wrought-iron lattice structure that offers breathtaking views of the city',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Eiffel Tower';
+
+UPDATE public.tourist_attraction
+SET "location"='Italy',
+    text_description='Imposing ancient amphitheater, the Colosseum is a monumental symbol of Rome''s rich history and a testament to the incredible engineering and architectural achievements of the Roman Empire.',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Colosseum';
+
+UPDATE public.tourist_attraction
+SET "location"='France',
+    text_description='Iconic Parisian landmark, the Eiffel Tower is a towering wrought-iron lattice structure that offers breathtaking views of the city',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Burj Khalifa';
+
+UPDATE public.tourist_attraction
+SET "location"='Saudi Arabia',
+    text_description='Stunning architectural marvel, Burj Khalifa is a soaring skyscraper that stands as the tallest building in the world, offering awe-inspiring views of Dubai''s futuristic skyline and the surrounding desert landscape.',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Burj Khalifa';
+
+UPDATE public.tourist_attraction
+SET "location"='Italy',
+    text_description='Stunning architectural marvel, Burj Khalifa is a soaring skyscraper that stands as the tallest building in the world, offering awe-inspiring views of Dubai''s futuristic skyline and the surrounding desert landscape.',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Cinque Terre';
+
+UPDATE public.tourist_attraction
+SET "location"='USA',
+    text_description='Breathtaking coastal paradise, Cinque Terre is a collection of five charming and colorful Italian fishing villages nestled along the rugged cliffs of the Ligurian coastline, offering stunning views of the Mediterranean Sea.',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Yellow Stone';
+
+UPDATE public.tourist_attraction
+SET "location"='',
+    text_description='',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Schonbrunn Palace';
+
+UPDATE public.tourist_attraction
+SET "location"='',
+    text_description='',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Giza Pyramids';
+
+
+UPDATE public.tourist_attraction
+SET "location"='',
+    text_description='',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Petra';
+
+
+UPDATE public.tourist_attraction
+SET "location"='',
+    text_description='',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Louvre';
+
+UPDATE public.tourist_attraction
+SET "location"='',
+    text_description='',
+    nr_visits=0, entry_price=20, discount=10, visiting_date='15-05-2023'
+WHERE name='Sagrada Familia';
